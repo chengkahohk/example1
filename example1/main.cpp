67dc3dd075555379f68efb847523bab43f725f8d
chengkahohk <46778468+chengkahohk@users.noreply.github.com>
2022-02-15 22:08:36 +0800

    Initial Commit

---
+++ example1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  example1
+//
+//  Created by Ka Ho Cheng on 15/2/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
