1343837536d4766b07d8ce1a45085800a303dad6
chengkahohk <46778468+chengkahohk@users.noreply.github.com>
2022-02-15 22:53:10 +0800

    master

--- example1/main.cpp
+++ example1/main.cpp
@@ -1,14 +1,7 @@
-//
-//  main.cpp
-//  example1
-//
-//  Created by Ka Ho Cheng on 15/2/2022.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main()
+{
+    std::cout << "Hello world! Ka Ho!" << std::endl;
+    std::cin.get();
 }
